93e6ab071e84f1bcf7812d441f693cb08c066503
Mauricio Gomes das Chagas <maumauriciog@hotmail.com>
2024-04-12 20:23:16 -0300

    buildFeatures with viewBinding = true v1.2

--- app/src/main/java/com/example/appgorjeta/MainActivity.kt
+++ app/src/main/java/com/example/appgorjeta/MainActivity.kt
@@ -1,13 +1,7 @@
 package com.example.appgorjeta
 
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.widget.Button
-import android.widget.EditText
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.appgorjeta.databinding.ActivityMainBinding
 
 
